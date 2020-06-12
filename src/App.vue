e320c6c042b7c25bc87e6c8c52a682d8e56bde42
John Kang <hphk.john@gmail.com>
2020-06-12 14:03:37 +0900

    Clear up code

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,6 @@ export default {
   },
   methods: {
     getDog() {
-      alert('hey')
       axios.get('https://dog.ceo/api/breeds/image/random')
         .then(response => {
           console.log(response.data.message)
